6172d5d933a57f696aa782e28e72d52242b358ad
zml <747765207@qq.com>
2023-12-20 14:52:47 +0800

    style: 格式化

--- theme/utils/index.js
+++ theme/utils/index.js
@@ -1,45 +1,44 @@
-
 /**
  * 一维数组转树状数组
  * arr: 要转换的一维数组
  * id: 唯一识别
  * pid: 父级唯一识别
  */
- export function arrayToJson(arr, id, pid, children = 'children') {
-  let tempArr = []
-  let tempObj = {}
+export function arrayToJson(arr, id, pid, children = 'children') {
+  let tempArr = [];
+  let tempObj = {};
   for (let i = 0, l = arr.length; i < l; i++) {
-    tempObj[arr[i][id]] = arr[i]
+    tempObj[arr[i][id]] = arr[i];
   }
   for (let i = 0, l = arr.length; i < l; i++) {
-    let key = tempObj[arr[i][pid]]
+    let key = tempObj[arr[i][pid]];
 
     if (key) {
       if (!key[children]) {
-        key[children] = []
-        key[children].push(arr[i])
+        key[children] = [];
+        key[children].push(arr[i]);
       } else {
-        key[children].push(arr[i])
+        key[children].push(arr[i]);
       }
     } else {
-      tempArr.push(arr[i])
+      tempArr.push(arr[i]);
     }
   }
-  return tempArr
+  return tempArr;
 }
 
 export function jsonToArray(nodes, children = 'children') {
-  let r = []
+  let r = [];
   if (Array.isArray(nodes)) {
     for (let i = 0, l = nodes.length; i < l; i++) {
-      r.push(nodes[i])
+      r.push(nodes[i]);
       if (Array.isArray(nodes[i][children]) && nodes[i][children].length > 0)
         //将children递归的push到最外层的数组r里面
-        r = r.concat(jsonToArray(nodes[i][children]))
-      delete nodes[i][children]
+        r = r.concat(jsonToArray(nodes[i][children]));
+      delete nodes[i][children];
     }
   }
-  return r
+  return r;
 }
 
 /**
@@ -47,37 +46,37 @@ export function jsonToArray(nodes, children = 'children') {
  */
 //返回传递给他的任意对象的类
 export function isClass(o) {
-  if (o === null) return 'Null'
-  if (o === undefined) return 'Undefined'
-  return Object.prototype.toString.call(o).slice(8, -1)
+  if (o === null) return 'Null';
+  if (o === undefined) return 'Undefined';
+  return Object.prototype.toString.call(o).slice(8, -1);
 }
 
 export function deepCopy(obj) {
   if (!obj) {
-    return null
+    return null;
   }
   let result,
-    oClass = isClass(obj)
+    oClass = isClass(obj);
   //确定result的类型
   if (oClass === 'Object') {
-    result = {}
+    result = {};
   } else if (oClass === 'Array') {
-    result = []
+    result = [];
   } else {
-    return obj
+    return obj;
   }
 
   for (let key in obj) {
-    if (obj.hasOwnProperty(key)) {
-      let copy = obj[key]
+    if (Object.prototype.hasOwnProperty.call(obj, key)) {
+      let copy = obj[key];
       if (isClass(copy) === 'Object') {
-        result[key] = deepCopy(copy) //递归调用
+        result[key] = deepCopy(copy); //递归调用
       } else if (isClass(copy) === 'Array') {
-        result[key] = deepCopy(copy)
+        result[key] = deepCopy(copy);
       } else {
-        result[key] = obj[key]
+        result[key] = obj[key];
       }
     }
   }
-  return result
+  return result;
 }
